c0e799808964ea4c958b4b0f76cc3340b83de053
SGuemghar <Syphax.GUEMGHAR@external.danone.com>
2025-12-23 22:43:42 +0100

    adding use msi in backends

--- network/backend.tf
+++ network/backend.tf
@@ -2,5 +2,6 @@ terraform {
   backend "azurerm" {
     key              = "sqlpoc.network.tfstate"
     use_azuread_auth = true
+    use_msi          = true
   }
 }
--- ops/backend.tf
+++ ops/backend.tf
@@ -2,5 +2,6 @@ terraform {
   backend "azurerm" {
     key              = "sqlpoc.ops.tfstate"
     use_azuread_auth = true
+    use_msi          = true
   }
 }
