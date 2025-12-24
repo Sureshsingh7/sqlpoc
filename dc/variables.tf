545d6421b8ead9586d15a19f60ccae01bfd45e6a
SGuemghar <Syphax.GUEMGHAR@external.danone.com>
2025-12-24 13:37:42 +0100

    adding tenant_id  and sub_id in DC worflow

--- dc/variables.tf
+++ dc/variables.tf
@@ -1,7 +1,7 @@
 variable "tenant_id" {
-    type        = string
-    description = "Azure Tenant ID"
-    default     = "0ebb4727-213e-46b9-8c4a-6611a5f157b0"
+  type        = string
+  description = "Azure Tenant ID"
+  default     = "0ebb4727-213e-46b9-8c4a-6611a5f157b0"
 }
 
 variable "subscription_id" {
