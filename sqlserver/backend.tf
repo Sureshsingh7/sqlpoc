ad8b53a4c559dc904e2c53466591c7facb20c8f5
Suresh Panwar <suresh.panwar@us.nestle.com>
2025-12-24 15:50:16 +0530

    updated the sqlserver

--- sqlserver/backend.tf
+++ sqlserver/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "azurerm" {
-    # resource_group_name  = "rg-fnz-poc-tfstate-se"
-    # storage_account_name = "stfnzpocdj522c"
-    # container_name       = "tfstate"
-    key              = "sqlpoc.sqlserver.tfstate"
-    use_msi          = true
+    resource_group_name  = "rg-fnz-poc-tfstate-se"
+    storage_account_name = "stfnzpocdj522c"
+    container_name       = "tfstate"
+    key                  = "sqlpoc.sqlserver.tfstate"
+    # use_msi              = true
     use_azuread_auth = true
   }
 }
