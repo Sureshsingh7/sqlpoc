8a78e31584c938096705ff3bd0d6b4edfa5103c7
Suresh Panwar <suresh.panwar@us.nestle.com>
2026-01-07 13:25:38 +0530

    Added keyvault private endpoint

--- ops/outputs.tf
+++ ops/outputs.tf
@@ -5,7 +5,3 @@ output "ops_key_vault_id" {
 output "kv_private_endpoint_id" {
   value = azurerm_private_endpoint.kv_pep.id
 }
-
-output "kv_private_endpoint_network_interface_ids" {
-  value = azurerm_private_endpoint.kv_pep.network_interface_ids
-}
