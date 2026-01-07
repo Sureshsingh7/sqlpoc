28896014a63b862d2f0b142e7869e1a286262ca5
Suresh Panwar <suresh.panwar@us.nestle.com>
2026-01-07 13:22:30 +0530

    Added keyvault private endpoint

--- sqlserver/data.tf
+++ sqlserver/data.tf
@@ -1,17 +1,3 @@
-# Fetch outputs from DC module for domain join
-data "terraform_remote_state" "dc" {
-  backend = "azurerm"
-
-  config = {
-    resource_group_name  = "rg-fnz-poc-tfstate-se"
-    storage_account_name = "stfnzpocdj522c"
-    container_name       = "tfstate"
-    key                  = "sqlpoc.dc.tfstate"
-    use_azuread_auth     = true
-    use_msi              = true
-  }
-}
-
 # Fetch outputs from network module
 data "terraform_remote_state" "network" {
   backend = "azurerm"
