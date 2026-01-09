5571f3007cff3184a61625a49485856ad4111c40
SGuemghar <Syphax.GUEMGHAR@external.danone.com>
2026-01-09 17:37:35 +0100

    fix IP addresses

--- sqlserver/variables.tf
+++ sqlserver/variables.tf
@@ -26,6 +26,7 @@ variable "sql_vm_names" {
 variable "sql_private_ips" {
   type        = list(string)
   description = "Static private IP addresses for SQL Server VMs"
+  default     = ["10.10.0.10", "10.10.0.74"]
 }
 
 
