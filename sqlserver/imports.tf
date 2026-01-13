7c99e3b059a26dda6b7d9dd6b14d9f4f27844226
Suresh Panwar <suresh.panwar@us.nestle.com>
2026-01-13 19:31:28 +0530

    Updated failover cluster creation step

--- sqlserver/imports.tf
+++ sqlserver/imports.tf
@@ -1,13 +1,13 @@
-# Import blocks for existing VM extensions
-# These were created but failed before being written to state
-# After successful import, this file can be deleted
+# # Import blocks for existing VM extensions
+# # These were created but failed before being written to state
+# # After successful import, this file can be deleted
 
-import {
-  to = azurerm_virtual_machine_extension.sql_disk_setup[0]
-  id = "/subscriptions/51595cc9-4191-4785-a757-15e45165d2a4/resourceGroups/rg-fnz-poc-sql-se/providers/Microsoft.Compute/virtualMachines/sql-primary/extensions/configure-sql-disks"
-}
+# import {
+#   to = azurerm_virtual_machine_extension.sql_disk_setup[0]
+#   id = "/subscriptions/51595cc9-4191-4785-a757-15e45165d2a4/resourceGroups/rg-fnz-poc-sql-se/providers/Microsoft.Compute/virtualMachines/sql-primary/extensions/configure-sql-disks"
+# }
 
-import {
-  to = azurerm_virtual_machine_extension.sql_disk_setup[1]
-  id = "/subscriptions/51595cc9-4191-4785-a757-15e45165d2a4/resourceGroups/rg-fnz-poc-sql-se/providers/Microsoft.Compute/virtualMachines/sql-secondary/extensions/configure-sql-disks"
-}
+# import {
+#   to = azurerm_virtual_machine_extension.sql_disk_setup[1]
+#   id = "/subscriptions/51595cc9-4191-4785-a757-15e45165d2a4/resourceGroups/rg-fnz-poc-sql-se/providers/Microsoft.Compute/virtualMachines/sql-secondary/extensions/configure-sql-disks"
+# }
