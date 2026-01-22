03e57dd926d0be8905428cadcc2b093c6bc9c5fb
SGuemghar <Syphax.GUEMGHAR@external.danone.com>
2026-01-22 16:52:51 +0100

    Allow enterprise-gen2 SQL image SKU

--- sqlserver/variables.tf
+++ sqlserver/variables.tf
@@ -182,8 +182,8 @@ variable "image_sku" {
   description = "Image SKU (enterprise, standard, express, or web)"
   default     = "standard"
   validation {
-    condition     = contains(["enterprise", "standard", "express", "web", "stddev-gen2", "standard-gen2"], var.image_sku)
-    error_message = "Image SKU must be one of: enterprise, standard, express, web, stddev-gen2, standard-gen2."
+    condition     = contains(["enterprise", "standard", "express", "web", "stddev-gen2", "standard-gen2", "enterprise-gen2"], var.image_sku)
+    error_message = "Image SKU must be one of: enterprise, standard, express, web, stddev-gen2, standard-gen2, enterprise-gen2."
   }
 }
 
