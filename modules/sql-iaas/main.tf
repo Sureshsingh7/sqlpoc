d97235938163deaa81da4ce6ca5a7563cf4e07dd
SGuemghar <Syphax.GUEMGHAR@external.danone.com>
2026-02-04 12:04:31 +0100

    Use private-only access for witness storage account
    - Set public_network_access_enabled=false (not true)
    - Remove network_rules to avoid service endpoint requirement
    - Private endpoint + DNS sufficient for cluster node access
    - SecurityControl tag 'ignore' ensures key access not blocked by policy

--- modules/sql-iaas/main.tf
+++ modules/sql-iaas/main.tf
@@ -76,20 +76,11 @@ module "witness_storage" {
   shared_access_key_enabled       = true
   allow_nested_items_to_be_public = false
   min_tls_version                 = "TLS1_2"
-  public_network_access_enabled   = true
+  public_network_access_enabled   = false
 
   tags = merge(var.tags, {
     SecurityControl = var.witness_storage_security_control_tag
   })
-
-  network_rules = {
-    default_action = "Deny"
-    bypass         = ["AzureServices"]
-    ip_rules       = []
-    virtual_network_subnet_ids = [
-      var.subnet_ids[0]
-    ]
-  }
 
   private_endpoints = {
     witness_blob = {
