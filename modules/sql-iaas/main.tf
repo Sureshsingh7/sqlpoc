b33189abec1cf796c7a2bd6aa4addd858e89ee1f
SGuemghar <Syphax.GUEMGHAR@external.danone.com>
2026-02-05 17:42:06 +0100

    Bump to v8 - Key Vault certificate sharing

--- modules/sql-iaas/main.tf
+++ modules/sql-iaas/main.tf
@@ -479,7 +479,7 @@ resource "azurerm_virtual_machine_run_command" "cluster_setup" {
 resource "azurerm_virtual_machine_run_command" "hadr_endpoint_setup" {
   for_each = var.is_ha ? local.vm_map : {}
 
-  name               = "hadr-endpoint-setup-v7"
+  name               = "hadr-endpoint-setup-v8"
   location           = var.location
   virtual_machine_id = module.sql_vm[each.key].resource_id
   depends_on         = [azurerm_virtual_machine_run_command.cluster_setup]
